d0039102a8627257c042cb731d6f59d25e34552d
ExizMan <yurasankin@yandex.ru>
2024-11-24 04:44:13 +0300

    fix

--- core/routes/wallet.py
+++ core/routes/wallet.py
@@ -37,7 +37,7 @@ async def get_wallets(token: str = Depends(oauth2_scheme), db: AsyncSession = De
     result = await db.execute(query)
     rows = result.scalars().all()
 
-    print(result)
+    print(rows)
 
     # Создаем словарь для группировки карточек по кошелькам
     wallet_dicts = []
