da4ce351cd1f37957cf315d3961f85220e865a56
ExizMan <yurasankin@yandex.ru>
2024-11-24 07:37:05 +0300

    chat

--- chatbot/run_web.py
+++ chatbot/run_web.py
@@ -1,12 +1,33 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Response
 from typing import List
 import uvicorn
+
 
 app = FastAPI()
 
 active_connections: List[WebSocket] = []
 messages: List[str] = []
 
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = Response()
+
+    if request.method == "OPTIONS":
+        response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
+        response.headers["Access-Control-Allow-Credentials"] = "true"
+        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+        return response
+
+    response = await call_next(request)
+
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+
+    return response
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
